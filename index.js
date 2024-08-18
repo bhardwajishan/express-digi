56f9a25daa3d4cc3906f9f5938b2c9652b72d5ba
bhardwajishan <bhardwajishan52@gmail.com>
2024-08-18 17:36:08 +0530

    First

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 const app = express();
 const port = 3000;
 
 app.use(express.json())
+const morganFormat = ":method :url :status :response-time ms";
 
 let teaData = [];
 let nextId= 1;
 
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
+
 // Add new tea
 app.post("/teas",(req,res)=>{
+    logger.info("A post is addded to tea")
     const {name,price}= req.body;
 
     const newTea={id:nextId++,name,price};
